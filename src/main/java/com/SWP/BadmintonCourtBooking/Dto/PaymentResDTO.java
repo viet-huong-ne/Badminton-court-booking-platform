2de8be7708e70d595d22b0a21b90e23966b748dc
viet-huong-ne <viethuong179@gmail.com>
2024-07-04 21:04:35 +0700

    them api getAllPayment va payment theo san again

--- src/main/java/com/SWP/BadmintonCourtBooking/Dto/PaymentResDTO.java
+++ src/main/java/com/SWP/BadmintonCourtBooking/Dto/PaymentResDTO.java
@@ -6,37 +6,44 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class PaymentResDTO implements Serializable {
-    private String status;
-    private String message;
-    private String URL;
+//    private String status;
+//    private String message;
+//    private String URL;
 
-    public String getStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getURL() {
-        return URL;
-    }
-
-    public void setURL(String URL) {
-        this.URL = URL;
-    }
+    //    public String getStatus() {
+//        return status;
+//    }
+//
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
+//
+//    public void setStatus(String status) {
+//        this.status = status;
+//    }
+//
+//    public String getURL() {
+//        return URL;
+//    }
+//
+//    public void setURL(String URL) {
+//        this.URL = URL;
+//    }
+    private String bankCode;
+    private BigDecimal paymentAmount;
+    private Date paymentDate;
+    private String transactionCode;
 }
 
